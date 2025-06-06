1e5807610e91c30f8034b84a3070f9ec39f97201
José Daniel Grayson <josedanielgrayson@proton.me>
2025-06-06 14:46:12 -0500

    build: add base property to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/mini-reto/",
   plugins: [react(), tailwindcss()],
 });
